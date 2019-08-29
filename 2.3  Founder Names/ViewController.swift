b76857d68a3330ab486fb528b62fce63d90586df
Braden Hamlin <braden@placefull.com>
2019-08-29 15:17:09 -0700

    Initial Commit

---
+++ 2.3  Founder Names/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2.3  Founder Names
+//
+//  Created by Braden Hamlin on 8/29/19.
+//  Copyright © 2019 Braden Hamlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
